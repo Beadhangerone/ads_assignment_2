a6dfce22114e004489fc780d3f09ed2461221efe
Beadhangerone <denm.2206@gmail.com>
2021-04-13 16:46:07 +0200

    Initial

---
+++ src/Tree.java
@@ -0,0 +1,17 @@
+public class Tree {
+    private Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+
+}
