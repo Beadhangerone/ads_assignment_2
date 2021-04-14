e3cbb6920ac12ff2c4682b7ed93af1bdc0067622
Julia <tankiewicz.julia@gmail.com>
2021-04-14 13:45:19 +0200

    julias findMin() and findMax()

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class BinarySearchTree extends BinaryTree {
+
+
+  public int findMin() throws Exception
+  {
+    ArrayList<Integer> elements = super.inOrder();
+    if(!elements.isEmpty())
+      return Collections.min(elements);
+
+    throw new Exception("Tree is empty");
+  }
+
+  public int findMax() throws Exception
+  {
+    ArrayList<Integer> elements = super.inOrder();
+    if(!elements.isEmpty())
+      return Collections.max(elements);
+
+    throw new Exception("Tree is empty");
+  }
 
 }
