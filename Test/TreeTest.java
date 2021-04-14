07840e10c4e20e7be54de8643354bd18ae0bfe79
roxi2601 <roxi260111@gmail.com>
2021-04-14 14:26:27 +0200

    insert() method

--- Test/TreeTest.java
+++ Test/TreeTest.java
@@ -1,5 +1,16 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TreeTest {
 
+    @Test
+    void insert(){
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(1);
+        binarySearchTree.insert(2);
+        binarySearchTree.insert(3);
+        binarySearchTree.insert(4);
+        binarySearchTree.insert(5);
+    }
 }
--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,3 +1,18 @@
 public class BinarySearchTree extends BinaryTree {
 
+    private BinaryTreeNode root;
+    public void insert(int element){
+        if(root==null)
+        {
+            root = new BinaryTreeNode(element);
+        }
+
+        if(element<root.getElement())
+        {
+            root.addLeftChild(element);
+        }
+        else if(element>root.getElement()){
+            root.addRightChild(element);
+        }
+    }
 }
