89956f14adf80dafb9472216de4ef6e2df3b784c
Julia <tankiewicz.julia@gmail.com>
2021-04-16 16:29:47 +0200

    findMax and findMin tested

--- Test/BinarySearchTreeTest.java
+++ Test/BinarySearchTreeTest.java
@@ -20,4 +20,42 @@ class BinarySearchTreeTest{
         treePrinter.printTree(bTree.getRoot());
     }
 
+    @Test
+    void findMin() throws Exception {
+        BinarySearchTree bTree = new BinarySearchTree();
+        BinaryTreePrint treePrinter = new BinaryTreePrint();
+
+        bTree.insert(3);
+        bTree.insert(10);
+        bTree.insert(1);
+        bTree.insert(6);
+        bTree.insert(8);
+        bTree.insert(11);
+        bTree.insert(2);
+        bTree.insert(4);
+        bTree.insert(7);
+
+
+        assertEquals(1,bTree.findMin().getElement());
+    }
+
+    @Test
+    void findMax() throws Exception {
+        BinarySearchTree bTree = new BinarySearchTree();
+        BinaryTreePrint treePrinter = new BinaryTreePrint();
+
+        bTree.insert(3);
+        bTree.insert(10);
+        bTree.insert(1);
+        bTree.insert(6);
+        bTree.insert(8);
+        bTree.insert(11);
+        bTree.insert(2);
+        bTree.insert(4);
+        bTree.insert(7);
+
+
+        assertEquals(11,bTree.findMax().getElement());
+    }
+
 }
